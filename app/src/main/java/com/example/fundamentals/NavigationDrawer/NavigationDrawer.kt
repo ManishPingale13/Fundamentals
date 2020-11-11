8fd9eef3d627976cf135252127b3e8c174b0f497
Manish Pingale <manishpingale2005@gmail.com>
2020-11-11 16:38:58 +0530

    Added Navigation Drawer Files.

---
+++ app/src/main/java/com/example/fundamentals/NavigationDrawer/NavigationDrawer.kt
@@ -0,0 +1,13 @@
+package com.example.fundamentals.NavigationDrawer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fundamentals.R
+
+class NavigationDrawer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_drawer)
+
+    }
+}
