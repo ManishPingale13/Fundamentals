d8915df838c90daff610885589a84296b33e7245
Manish Pingale <manishpingale2005@gmail.com>
2020-11-11 17:30:55 +0530

    Navigation Drawer Code Complete.

--- app/src/main/java/com/example/fundamentals/NavigationDrawer/NavigationDrawer.kt
+++ app/src/main/java/com/example/fundamentals/NavigationDrawer/NavigationDrawer.kt
@@ -1,13 +1,44 @@
 package com.example.fundamentals.NavigationDrawer
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fundamentals.R
+import com.example.fundamentals.R.string.close
+import com.example.fundamentals.R.string.open
+import kotlinx.android.synthetic.main.activity_navigation_drawer.*
 
 class NavigationDrawer : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation_drawer)
 
+        toggle = ActionBarDrawerToggle(this, drawerlay, open, close)
+        drawerlay.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.i1 -> Toast.makeText(this, "Clicked First Item", Toast.LENGTH_SHORT).show()
+
+                R.id.i2 -> Toast.makeText(this, "Clicked Second Item", Toast.LENGTH_SHORT).show()
+
+                R.id.i3 -> Toast.makeText(this, "Clicked Third Item", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
